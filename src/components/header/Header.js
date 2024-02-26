9a93ce77c1956c62ba17d037c9ff51defbc2954e
Jordana Alves Carneiro <jordanaalvescarneirou@gmail.com>
2024-02-26 14:01:26 -0300

    first commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import Title from "../title/Title"
+
+function Header (){
+    return (
+        <header>
+            <Title>Hello world</Title>
+            <Title>Seja Dev</Title>
+            <Title>Outro titulo</Title>
+        </header>
+    )
+}
+
+export default Header
